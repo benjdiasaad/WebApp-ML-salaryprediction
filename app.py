9c9335ea4ff6105d24bc4c358832d55eb4d5a78b
Benjdia Saad <benjdiasaad97@gmail.com>
2021-05-03 04:01:46 +0000

    Créer un fichier py pour prédire salaire

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from predict_page import show_predict_page
+
+show_predict_page()
---
+++ predict_page.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+
+    st.write("""### We need some information to predict the salary""")
